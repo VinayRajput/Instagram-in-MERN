87364eabb2acfdb15c7ac32ef94f4f84d966a5a3
Vinay Rajput <vinkrins@gmail.com>
2020-06-24 19:37:24 +0530

    removed comment values from key file

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,4 @@
 module.exports = {
-   MONGOURI:process.env.MONGOURI//"mongodb+srv://instagramClone:r@@tPass@cluster0-hpn47.mongodb.net/test?retryWrites=true&w=majority"
-   ,
-   JWT_SECRET:process.env.JWT_SECRET//"xfxf09091010"
+   MONGOURI:process.env.MONGOURI,
+   JWT_SECRET:process.env.JWT_SECRET
 }
