226587096d48e1beb8c560c0505c91f088cb32ab
Vinay Rajput <vinkrins@gmail.com>
2020-06-27 03:16:22 +0530

    Small prod config changes

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,6 @@ module.exports = {
    JWT_SECRET:process.env.JWT_SECRET,
    SENDGRID_API : process.env.SENDGRID_API,//`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`,
    BASE_URL:process.env.BASE_URL,//"https://an-instagram.herokuapp.com"
-   SERVER_EMAIL:"vinay@vinayrajput.com",
+   SERVER_EMAIL:process.env.SERVER_EMAIL,
    TOKEN_EXPIRY_TIME: process.env.TOKEN_EXPIRY_TIME//1000 * 60 * 60 //1 hour -> miliseconds * sec * minutes,
 }
