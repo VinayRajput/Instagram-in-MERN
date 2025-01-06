294220d8f942c80f33cfdc590c46cb75526f1bd5
Vinay Rajput <vnaykrajput@gmail.com>
2025-01-06 23:33:45 +0530

    few updates

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const mongoose = require("mongoose");
 const {MONGOURI} = require("./config/keys");
 
@@ -33,7 +33,7 @@ if(process.env.NODE_ENV == "production"){
 
 app.get("/", (req,res)=>{
    console.log('home')
-   res.send("Hello world!")
+   res.send("Apis for An-Instagram clone")
 })
 
 app.listen(PORT,()=>{
